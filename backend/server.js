a982e196222c102ac01d9c96f09db12c39e6ce40
Joao Ferreira <joaoferreira.miecom@gmail.com>
2022-04-23 17:48:58 +0100

    [14. App Deployment] - Prepare deployment

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 // @Allows json data in body
 app.use(express.json());
 
@@ -37,6 +33,16 @@ app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve();
 // Making uploads folder static so it can be accessed by the browser
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
